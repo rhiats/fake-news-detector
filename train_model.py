82b6626c68f546b05165decaba05573947b6e4c4
Rhia Singh <rhia.singh22@gmail.com>
2026-06-28 21:46:48 -0400

    pre process data function

--- train_model.py
+++ train_model.py
@@ -21,6 +21,67 @@ def load_data(fake_path, true_path):
 
     return df
 
+def clean_text(text):
+    """
+        Pre process text:
+            - lowercase
+            - remore hyperlinks
+            - only consider lowercase letters 
+            - remove newssource artifacts
+
+        @p: text (str)
+
+        @r: text (str) cleaned text
+
+    """
+
+    text = text.lower()
+
+    # remove urls
+    text = re.sub(r"http\S+", "", text)
+
+    # remove numbers + punctuation
+    text = re.sub(r"[^a-z\s]", "", text)
+
+    # remove known news source artifacts
+    leakage_tokens = [
+        "reuters",
+        "washington",
+        "breakingnews",
+        "ap",
+        "associatedpress",
+    ]
+
+    for token in leakage_tokens:
+        text = text.replace(token, "")
+
+    # collapse extra whitespace
+    text = re.sub(r"\s+", " ", text).strip()
+
+    return text
+
+def pre_process_data(data_df,clean_text):
+    """
+        Pre process the text in the dataset:
+            - lowercase
+            - remore hyperlinks
+            - only consider lowercase letters 
+            - remove newssource artifacts
+
+        @p: data_df(dataframe) - full data
+        @p: clean_text - function to clean the text in the dataset
+
+        @r: data_df(dataframe) - includes cleaned text
+
+    """
+
+    data_df["clean_text"] = data_df["title"] + " " + data_df["text"]
+    data_df["clean_text"] = data_df["clean_text"].apply(clean_text)
+
+    return data_df
+
+
+
 def split_data(data_df):
     """
         Train Test split into training and test file
