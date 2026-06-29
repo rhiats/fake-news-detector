1d33177e54c9cfd32529d5683def910fb5ed430e
Rhia Singh <rhia.singh22@gmail.com>
2026-06-28 21:34:56 -0400

    split data function

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def load_data(fake_path, true_path):
     """
@@ -19,3 +20,20 @@ def load_data(fake_path, true_path):
     df = pd.concat([fake_df, true_df], ignore_index=True)
 
     return df
+
+def split_data(data_df):
+    """
+        Train Test split into training and test file
+
+        @p: data_df(dataframe) - full data
+        @r: train set (dataframe), test set (dataframe) 
+    """
+
+    train_df, test_df = train_test_split(
+    data_df, 
+    test_size=0.2, 
+    stratify=data_df['label'],
+    random_state=42)
+
+    return train_df,test_df
+
