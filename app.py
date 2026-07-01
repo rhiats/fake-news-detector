38400ddf67ffa0241edaa5e5f6f4cabad2827840
Rhia Singh <rhia.singh22@gmail.com>
2026-07-01 18:12:02 -0400

    Basic html UI skeleton

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
 import predict as pred
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 model, vectorizer = pred.load_model()
 
 # Define the root endpoint
-@app.get("/")
-def read_root():
-    return {"message": "Fake News Detector API"}
+#@app.get("/")
+#def read_root():
+#    return {"message": "Fake News Detector API"}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 class ArticleRequest(BaseModel):
     text: str
