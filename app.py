ccf6abcf775dded6eede3edc93879927a203fdb6
Rhia Singh <rhia.singh22@gmail.com>
2026-07-01 18:12:54 -0400

    add static files inmport to app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import predict as pred
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
